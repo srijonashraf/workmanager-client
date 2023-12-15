b089621f3a85aa42f0a953b44eb83bbb27077fdd
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2023-12-15 14:41:59 +0600

    style folder added

--- src/page/TaskPage.jsx
+++ src/page/TaskPage.jsx
@@ -1,27 +1,54 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Card, Container} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
+import {listTaskRequest} from "../apiRequest/apiRequest.js";
 
 const TaskPage = () => {
+    let [data, setData] = useState([]);
+    let [change, setChange] = useState(0);
+
+    useEffect(() => {
+        (async () => {
+            let res = await listTaskRequest();
+            setData(res);
+        })()
+    }, [change]);
+
+    // const onDelete = async (id) => {
+    //     let res = await deleteTaskRequest(id);
+    //     if (res) {
+    //         toast.success("Delete completed");
+    //         setChange(new Date().getTime())
+    //     } else {
+    //         toast.error("Delete fail")
+    //     }
+    // }
+
     return (
         <div>
             <Container className="mt-5">
-                <Card style={{width: '20rem'}}>
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text>
-                        <p className="btn btn-success btn-sm">Status</p>
-                        <div className="action justify-content-end d-flex gap-2">
-                        <Button variant="primary btn-sm">Edit</Button>
-                        <Button variant="danger btn-sm">Delete</Button>
-                        <Button variant="dark btn-sm">Update</Button>
+                {data.map((task) => (
+                    <Card key={task.id} style={{width: '20rem'}} className="mb-3">
+                        <Card.Body>
+                            <Card.Title>{task.workTitle}</Card.Title>
+                            <Card.Text>{task.taskDetails}</Card.Text>
+                            <div className="d-flex align-items-center gap-2">
+                                <p className={`btn btn-${task.workStatus === 'Done' ? 'success' : 'warning'} btn-sm`}>
+                                    {task.workStatus}
+                                </p>
+                                <p className="text-muted sm-text">Last Updated: {task.lastUpdated}</p>
+                            </div>
 
-                        </div>
-                    </Card.Body>
-                </Card>
+                            <div className="action justify-content-end d-flex gap-2">
+                                <Button variant="primary btn-sm">Edit</Button>
+                                <Button variant="danger btn-sm" onClick={() => onDelete(task.id)}>
+                                    Delete
+                                </Button>
+                                <Button variant="dark btn-sm">Update</Button>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                ))}
             </Container>
         </div>
     );
