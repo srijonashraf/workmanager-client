16cb2d3d3bdcf8b9435eebe8c18b4b589282416e
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2023-12-19 01:26:18 +0600

    Routing Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,45 @@
-import AppNavbar from "./components/AppNavbar";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import HomePage from "./page/HomePage.jsx";
 import TaskPage from "./page/TaskPage.jsx";
 import ProjectPage from "./page/ProjectPage.jsx";
-import LoginPage from './page/LoginPage';
+import LoginPage from "./page/LoginPage";
+import AppNavbar from "./components/AppNavbar";
+import { isLoggedIn } from "./helper/SessionHelper.js";
+import Error from "./components/404.jsx";
 
 function App() {
+  const loggedIn = isLoggedIn();
+
+  if (loggedIn) {
     return (
+      <Router>
         <div>
-            <BrowserRouter>
-                <AppNavbar/>
-                <Routes>
-                    <Route path="/" element={<LoginPage/>}/>
-                    <Route path="/task" element={<TaskPage/>}/>
-                    <Route path="/project" element={<ProjectPage/>}/>
-                    <Route path="/dashboard" element={<HomePage/>}/>
-                </Routes>
-            </BrowserRouter>
+          <AppNavbar />
+          <Routes>
+            <Route path="/task" element={<TaskPage />} />
+            <Route path="/project" element={<ProjectPage />} />
+            <Route path="/dashboard" element={<HomePage />} />
+            <Route path="*" element={<Navigate to="/dashboard" />} />
+          </Routes>
         </div>
+      </Router>
     );
+  } else {
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          {/* Other Route will also redirect to LoginPage */}
+        </Routes>
+      </Router>
+    );
+  }
 }
 
 export default App;
