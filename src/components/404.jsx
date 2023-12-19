68691462d9b39ded6ea41570dab0a14c1be45717
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2023-12-19 12:22:01 +0600

    design fixed

--- src/components/404.jsx
+++ src/components/404.jsx
@@ -3,9 +3,13 @@ import { NavLink } from 'react-router-dom';
 
 const Error = () => {
     return (
-        <div>
-            404 Error!
-            <NavLink className="btn btn-primary" to={"/login"}>Go Back</NavLink>
+        <div className='container d-flex flex-column min-vh-100 align-items-center justify-content-center'>
+            <div className='row'>
+                <div className='col-md-12 col-lg-12 text-center'>
+                    <h1>404 Error!</h1>
+                    <NavLink className="btn btn-dark" to={"/"}>{`<< `}Go Back</NavLink>
+                </div>
+            </div>
         </div>
     );
 };
