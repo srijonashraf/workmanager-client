c5608a8c8b052d728bd44fe6570cf60e56cb4fb5
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2023-12-19 15:06:26 +0600

    Register Request Added

--- src/apiRequest/apiRequest.js
+++ src/apiRequest/apiRequest.js
@@ -2,10 +2,10 @@ import axios from "axios";
 import { useCookies } from "react-cookie";
 import { setToken, setLoggedIn, isLoggedIn } from "../helper/SessionHelper";
 const BASE_URL = "https://workmanager-srijonashraf.vercel.app/api/v1";
+
 function UserLoginRequest(email, password) {
   let URL = BASE_URL + "/login";
   let PostBody = { email: email, password: password };
-
   return axios
     .post(URL, PostBody)
     .then((res) => {
@@ -25,4 +25,35 @@ function UserLoginRequest(email, password) {
     });
 }
 
-export { UserLoginRequest }; // Correct export statement
+function UserRegistrationRequest(formValues) {
+  let URL = BASE_URL + "/registration";
+  let PostBody = {
+    employeeId: formValues.employeeId,
+    email: formValues.email,
+    firstName: formValues.firstName,
+    lastName: formValues.lastName,
+    mobile: formValues.mobile,
+    password: formValues.password,
+    address: formValues.address,
+    position: formValues.position,
+    department: formValues.department,
+  };
+
+  return axios
+    .post(URL, PostBody)
+    .then((res) => {
+      if (res.data.status === "success") {
+        console.log("Registration Successful");
+        return true;
+      } else {
+        console.log("Registration Failed");
+        return false;
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      return false;
+    });
+}
+
+export { UserLoginRequest, UserRegistrationRequest };
