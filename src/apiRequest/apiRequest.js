9698e139e35a3fa0e9a83d1dc9fbb36a94a9eca3
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2023-12-20 00:49:34 +0600

    AddNewTask and AllTask Added

--- src/apiRequest/apiRequest.js
+++ src/apiRequest/apiRequest.js
@@ -1,33 +1,40 @@
 import axios from "axios";
-import { useCookies } from "react-cookie";
-import { setToken, setLoggedIn, isLoggedIn } from "../helper/SessionHelper";
+import {
+  setToken,
+  getToken,
+  setLoggedIn,
+  isLoggedIn,
+} from "../helper/SessionHelper";
+
 const BASE_URL = "https://workmanager-srijonashraf.vercel.app/api/v1";
 
 function UserLoginRequest(email, password) {
-  let URL = BASE_URL + "/login";
-  let PostBody = { email: email, password: password };
+  const URL = BASE_URL + "/login";
+  const postBody = { email, password };
+
   return axios
-    .post(URL, PostBody)
+    .post(URL, postBody)
     .then((res) => {
       if (res.data.status === "success") {
         setToken(res.data.token);
         setLoggedIn(true);
-        console.log(isLoggedIn());
+        console.log("Login Successful");
         return true;
       } else {
         setLoggedIn(false);
+        console.log("Login Failed");
         return false;
       }
     })
     .catch((err) => {
-      console.log(err);
+      console.error(err);
       return false;
     });
 }
 
 function UserRegistrationRequest(formValues) {
-  let URL = BASE_URL + "/registration";
-  let PostBody = {
+  const URL = BASE_URL + "/registration";
+  const postBody = {
     employeeId: formValues.employeeId,
     email: formValues.email,
     firstName: formValues.firstName,
@@ -40,7 +47,7 @@ function UserRegistrationRequest(formValues) {
   };
 
   return axios
-    .post(URL, PostBody)
+    .post(URL, postBody)
     .then((res) => {
       if (res.data.status === "success") {
         console.log("Registration Successful");
@@ -51,9 +58,47 @@ function UserRegistrationRequest(formValues) {
       }
     })
     .catch((err) => {
-      console.log(err);
+      console.error(err);
       return false;
     });
 }
 
-export { UserLoginRequest, UserRegistrationRequest };
+function AddNewTask(taskTitle, taskDescription) {
+  const URL = BASE_URL + "/createWork";
+  const postBody = {
+    workTitle: taskTitle,
+    workDescription: taskDescription,
+    workStatus: "Pending",
+  };
+
+  return axios
+    .post(URL, postBody, { headers: { token: getToken() } })
+    .then((res) => {
+      if (res.data.status === "success") {
+        console.log("Data Added.");
+        return true;
+      } else {
+        console.log("Data Not Added.");
+        return false;
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      return false;
+    });
+}
+
+function AllTask() {
+  const URL = BASE_URL + "/allWork";
+  return axios
+    .get(URL, { headers: { token: getToken() } })
+    .then((res) => {
+      return res; // Return the entire response object
+    })
+    .catch((err) => {
+      console.error(err);
+      return false;
+    });
+}
+
+export { UserLoginRequest, UserRegistrationRequest, AddNewTask, AllTask };
