8499fcbc0c429b7fdf018848b5b1dfa3c128804a
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2023-12-20 13:11:09 +0600

    modified

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -1,16 +1,13 @@
+import React from "react";
 import Button from "react-bootstrap/Button";
-import { Container, Nav, Navbar, Form, Row, Col } from "react-bootstrap";
+import { Container, Nav, Navbar, Form, InputGroup, FormControl } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import { clearSessions } from "../helper/SessionHelper";
+
 const AppNavbar = () => {
   return (
     <div>
-      <Navbar
-        collapseOnSelect
-        expand="lg"
-        className="bg-dark-subtle"
-        data-bs-theme="dark"
-      >
+      <Navbar collapseOnSelect expand="lg" className="bg-dark-subtle" data-bs-theme="dark">
         <Container className="my-2 my-lg-0">
           <NavLink to={"/"} className="navbar-brand">
             Work Manager
@@ -18,9 +15,6 @@ const AppNavbar = () => {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto justify-content-center flex-grow-1">
-              {/* <NavLink className="nav-link" to="/dashboard">
-                Dashboard
-              </NavLink> */}
               <NavLink className="nav-link" to="/task">
                 New Task
               </NavLink>
@@ -28,31 +22,15 @@ const AppNavbar = () => {
                 All Task
               </NavLink>
             </Nav>
-            <Nav>
-              <Form>
-                <Row>
-                  <Col xs="auto">
-                    <Form.Control
-                      type="text"
-                      placeholder="Search"
-                      className="mr-sm-2"
-                    />
-                  </Col>
-                  <Col xs="auto"  className="mb-3 mb-sm-0">
-                    <Button type="submit">Search</Button>
-                  </Col>
-                  <Col xs="auto">
-                    <button
-                      onClick={clearSessions}
-                      type="button"
-                      className="btn btn-outline-danger"
-                    >
-                      Logout
-                    </button>
-                  </Col>
-                </Row>
-              </Form>
-            </Nav>
+            <Form className="d-flex align-items-center">
+              <InputGroup className="me-2">
+                <FormControl type="text" placeholder="Search" />
+                <Button variant="primary">Search</Button>
+              </InputGroup>
+              <Button onClick={clearSessions} variant="danger">
+                Logout
+              </Button>
+            </Form>
           </Navbar.Collapse>
         </Container>
       </Navbar>
