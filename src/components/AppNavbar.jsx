d6762a775d38cd84833c0ffa0302a308f8b3d88d
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2023-12-21 03:24:58 +0600

    [TEMPORARY] UserEmail will be visible

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
-import { Container, Nav, Navbar, Form, InputGroup, FormControl } from "react-bootstrap";
+import {
+  Container,
+  Nav,
+  Navbar,
+  Form,
+  InputGroup,
+  FormControl,
+} from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import { clearSessions } from "../helper/SessionHelper";
+import { getUserEmail } from "../helper/SessionHelper";
 
+const userEmail = getUserEmail();
 const AppNavbar = () => {
   return (
     <div>
-      <Navbar collapseOnSelect expand="lg" className="bg-dark-subtle" data-bs-theme="dark">
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        className="bg-dark-subtle"
+        data-bs-theme="dark"
+      >
         <Container className="my-2 my-lg-0">
           <NavLink to={"/"} className="navbar-brand">
             Work Manager
@@ -30,6 +44,7 @@ const AppNavbar = () => {
               <Button onClick={clearSessions} variant="danger">
                 Logout
               </Button>
+              <h6 className="text-light ms-3 ">{userEmail}</h6>
             </Form>
           </Navbar.Collapse>
         </Container>
