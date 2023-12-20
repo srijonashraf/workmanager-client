452b585dda45a2ec36d574105aec6b96c3e00572
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2023-12-20 13:12:12 +0600

    Task List, Status Change, Delete Task Added

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,59 +1,173 @@
 import React, { useEffect, useState } from "react";
-import { Container, Card, Row, Col, Button } from "react-bootstrap";
-import { AllTask } from "../apiRequest/apiRequest";
+import { Container, Card, Row, Col, Button, Modal } from "react-bootstrap";
+import {
+  AllTask,
+  DeleteTask,
+  UpdateTaskStatus,
+} from "../apiRequest/apiRequest";
+import { errorToast, successToast } from "../helper/ToasterHelper";
+import { Toaster } from "react-hot-toast";
 
 const TaskList = () => {
   const [tasks, setTasks] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedTaskId, setSelectedTaskId] = useState(null);
+  const [selectedTaskStatus, setSelectedTaskStatus] = useState("Done"); // Set the initial status here
 
   useEffect(() => {
-    fetchAllTasks();
+    FetchAllTasks();
   }, []);
 
-  const fetchAllTasks = async () => {
+  const DeleteTaskRequest = async (id) => {
+    try {
+      const response = await DeleteTask(id);
+      if (response) {
+        successToast("Task deleted successfully");
+        FetchAllTasks();
+      } else {
+        errorToast("Failed to delete task");
+      }
+    } catch (err) {
+      console.log("Failed to reach server");
+    }
+  };
+
+  const UpdateTaskStatusRequest = async (taskId, status) => {
+    try {
+      const response = await UpdateTaskStatus(taskId, status);
+      if (response) {
+        successToast("Task status updated");
+        FetchAllTasks();
+      } else {
+        errorToast("Failed to update");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const FetchAllTasks = async () => {
     try {
       setLoading(true);
       const response = await AllTask();
 
       if (response && response.data && response.status === 200) {
-        setTasks(response.data.data); // Assuming the actual data is in the 'data' property
-        console.log("Tasks fetched successfully");
+        setTasks(response.data.data);
       } else {
-        setError("Invalid response format. Please try again.");
         console.error("Invalid response format:", response);
+        setTasks([]);
       }
     } catch (error) {
       console.error("Failed to fetch tasks", error);
-      setError("Failed to fetch tasks. Please try again.");
+      setTasks([]);
     } finally {
       setLoading(false);
     }
   };
 
+  const handleUpdateStatus = () => {
+    if (selectedTaskId) {
+      UpdateTaskStatusRequest(selectedTaskId, selectedTaskStatus);
+      setShowModal(false);
+    }
+  };
+
   return (
     <Container fluid className="mt-3">
+      <Toaster position="top-right"></Toaster>
       <Row className="row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4">
         {loading ? (
           <Col className="mb-3">
             <p>Loading...</p>
           </Col>
-        ) : error ? (
-          <Col className="mb-3">
-            <p style={{ color: "red" }}>{error}</p>
-          </Col>
         ) : (
           tasks.map((task) => (
             <Col key={task._id} className="mb-3">
-              <Card className="h-100 shadow border-0">
-                <Card.Body>
+              <Card className="h-100 shadow border-0 d-flex flex-column">
+                <Card.Body className="d-flex flex-column animated fadeInUp">
                   <h5>{task.workTitle}</h5>
                   <p>{task.workDescription}</p>
-                  <Button className="text-white btn btn-success btn-sm me-2">Status: {task.workStatus}</Button>
-                  <Button className="btn-sm btn-dark me-2">Edit</Button>
-                  <Button className="btn-sm btn-danger">Delete</Button>
-                  <p className="text-muted sm-text float-end mt-2">Last Updated: {task.updatedAt}</p>
-                  
+                  <Card.Footer className="mt-auto border-top animated fadeInRight">
+                    {(() => {
+                      let variant = "";
+                      if (task.workStatus === "Done") {
+                        variant = "success";
+                      } else if (task.workStatus === "In Progress") {
+                        variant = "warning";
+                      } else if (task.workStatus === "Cancelled") {
+                        variant = "danger";
+                      } else {
+                        variant = "primary";
+                      }
+
+                      return (
+                        <>
+                          <Button
+                            variant={variant}
+                            className="text-white rounded-1 btn-sm me-2"
+                            onClick={() => {
+                              setShowModal(true);
+                              setSelectedTaskId(task._id);
+                            }}
+                          >
+                            {task.workStatus}
+                          </Button>
+
+                          {/* Modal for updating task status */}
+                          <Modal
+                            show={showModal}
+                            onHide={() => setShowModal(false)}
+                          >
+                            <Modal.Header closeButton>
+                              <Modal.Title>Update Task Status</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                              <p>Select the new status for the task:</p>
+                              <select
+                                className="form-select"
+                                onChange={(e) =>
+                                  setSelectedTaskStatus(e.target.value)
+                                }
+                                value={selectedTaskStatus}
+                              >
+                                <option value="Done">Done</option>
+                                <option value="In Progress">In Progress</option>
+                                <option value="Cancelled">Cancelled</option>
+                              </select>
+                            </Modal.Body>
+                            <Modal.Footer>
+                              <Button
+                                variant="secondary"
+                                onClick={() => setShowModal(false)}
+                              >
+                                Close
+                              </Button>
+                              <Button
+                                variant="primary"
+                                onClick={handleUpdateStatus}
+                              >
+                                Save Changes
+                              </Button>
+                            </Modal.Footer>
+                          </Modal>
+                        </>
+                      );
+                    })()}
+                    {/* Edit Button is disabled TEMPORARY */}
+                    <Button className="d-none btn-sm rounded-1 btn-dark me-2"> 
+                      Edit
+                    </Button>
+                    <Button
+                      onClick={() => DeleteTaskRequest(task._id)}
+                      className="btn-sm rounded-1 btn-danger"
+                    >
+                      Delete
+                    </Button>
+                    <p className="text-muted sm-text float-end mt-2">
+                      Edited: {task.updatedAt}
+                    </p>
+                  </Card.Footer>
                 </Card.Body>
               </Card>
             </Col>
