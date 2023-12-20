2a9d4cf4739115cf5200ad856cc050e01d17a2b1
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2023-12-21 04:29:00 +0600

    [TEMPORARY} Wave animatation added and email address shown

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Container, Card, Row, Col } from "react-bootstrap";
 import { FetchTaskCount } from "../apiRequest/apiRequest";
+import { getUserEmail } from "../helper/SessionHelper";
 
+const userEmail = getUserEmail();
 const Dashboard = () => {
   const [taskCounts, setTaskCounts] = useState([]);
 
@@ -21,14 +23,22 @@ const Dashboard = () => {
   return (
     <div>
       <Container>
-        <h2 className="mt-3">Dashboard</h2>
+        <h2 className="mt-3 d-flex align-items-baseline gap-2">
+          Hi!{" "}
+          <span className="h4">
+            {`(${userEmail}) `}
+            <span className="blog-title-emoji">👋</span>
+          </span>
+        </h2>
         <Row xs={12}>
           {taskCounts.map((statusCount) => (
             <Col key={statusCount.status} md={4} className="mb-3">
               <Card className="border-0 shadow">
                 <Card.Body>
                   <Card.Title>{statusCount.status}</Card.Title>
-                  <Card.Text className="fw-bold text-muted">{statusCount.count}</Card.Text>
+                  <Card.Text className="fw-bold text-muted">
+                    {statusCount.count}
+                  </Card.Text>
                 </Card.Body>
               </Card>
             </Col>
