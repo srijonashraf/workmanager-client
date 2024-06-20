8911eb754405012e9354b74b3df326a077478021
Srijon Ashraf <72869452+srijonashraf@users.noreply.github.com>
2024-06-20 22:53:36 +0600

    fix: netlify building issue

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "../src/assets/css/style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../src/assets/css/animate.min.css";
 import "../src/assets/css/dropdownmenu.css";
-import "../src/assets/css/worklist.css"
+import "../src/assets/css/work.css"
 import "react-quill/dist/quill.snow.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
